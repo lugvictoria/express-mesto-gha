69c92a132fd0d7201ae3aced9c983db773291ec2
lugvictoria <victory.senty@yandex.ru>
2023-05-29 17:48:04 +0300

    13 спринт создать модели

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
 
 app.get('/', () => {
   console.log('get request recieved!');
 });
-
 app.listen(PORT);

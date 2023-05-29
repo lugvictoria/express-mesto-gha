60e5f8478238a3c8e5d4e0195f4fd55fcf354d00
lugvictoria <victory.senty@yandex.ru>
2023-05-29 21:54:10 +0300

    13 спринт исправить часть ошибок

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,12 +1,14 @@
 function handleError(err, req, res) {
-  if (err.name === 'ValidationError') {
+  console.log(err.name);
+
+  if (err.name === 'ValidationError' || err.name === 'CastError') {
     res.status(400).send({
       message: 'Переданы некорректные данные',
     });
     return;
   }
 
-  if (err.name === 'CastError') {
+  if (err.name === 'NotFoundError') {
     res.status(404).send({
       message: 'Объект не найден',
     });

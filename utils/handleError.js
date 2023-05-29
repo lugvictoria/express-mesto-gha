23232fb9e73341af7e1f253853aaa087b4aa5b49
lugvictoria <victory.senty@yandex.ru>
2023-05-29 22:39:28 +0300

    13 спринт обработка ошибок валидации данных

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,22 +1,25 @@
 function handleError(err, req, res) {
-
-  if (err.name === 'ValidationError' || err.name === 'CastError') {
+  if (err.name === 'CastError') {
     res.status(400).send({
-      message: 'Переданы некорректные данные',
+      message: 'Неверный формат переданных данных',
     });
     return;
   }
 
+  if (err.name === 'ValidationError') {
+    res.status(400).send({
+      message: err.message,
+    });
+    return;
+  }
   if (err.name === 'NotFoundError') {
     res.status(404).send({
       message: err.message,
     });
     return;
   }
-
   res.status(500).send({
     message: 'Не получилось обработать запрос',
   });
 }
-
 module.exports = { handleError };

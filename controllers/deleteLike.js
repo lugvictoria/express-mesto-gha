97a72c36fc35ddeed6a57904322132d021132a5f
lugvictoria <victory.senty@yandex.ru>
2023-05-29 22:01:34 +0300

    13 спринт обработка ошибки с несуществующей карточкой

--- controllers/deleteLike.js
+++ controllers/deleteLike.js
@@ -1,18 +1,23 @@
 const { Card } = require('../models/card');
 const { handleError } = require('../utils/handleError');
-
 async function deleteLike(req, res) {
   try {
     const userId = req.user._id;
     const card = await Card.findByIdAndUpdate(
       req.params.cardId,
       { $pull: { likes: userId } }, // убрать _id из массива, если он есть
-      { new: true },
-    );
-    res.send(card);
-  } catch (err) {
-    handleError(err, req, res);
-  }
-}
+      { new: true }
+      );
 
-module.exports = { deleteLike };
+      if (!card) {
+        const error = new Error('Карточка не найдена');
+        error.name = 'NotFoundError';
+        throw error;
+      }
+
+      res.send(card);
+    } catch (err) {
+      handleError(err, req, res);
+    }
+  }
+  module.exports = { deleteLike };

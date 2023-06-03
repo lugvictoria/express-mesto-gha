61180bcaefb6c50e70894b3cb4188ebd0b511890
lugvictoria <victory.senty@yandex.ru>
2023-06-03 20:51:32 +0300

    13 проектная fix

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb'), {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-};
+});
 app.use(helmet());
 // временное решение авторизации пользователя
 app.use((req, res, next) => {

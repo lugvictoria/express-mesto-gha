ea377e8b42244936e42ba0cdd62efe547f879c89
lugvictoria <victory.senty@yandex.ru>
2023-06-03 20:57:24 +0300

    13 проектная fix 2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { handleError } = require('../utils/handleError');
-const { users } = require('./users');
-const { cards } = require('./cards');
+const users = require('./users');
+const cards = require('./cards');
 
 const router = express.Router();
 
-router.use('/users', users);
-router.use('/cards', cards);
-router.all('*', (req, res) => {
+router.use(users);
+router.use(cards);
+router.use((req, res) => {
   const err = new Error('Неверный адрес запроса');
   err.name = 'NotFoundError';
   handleError(err, req, res);

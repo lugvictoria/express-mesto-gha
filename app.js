4dfc347c5d0f4c4f7fe7cf5bac00a019c935cc0d
lugvictoria <victory.senty@yandex.ru>
2023-06-08 20:19:35 +0300

    14 спринт добавить обработчик ошибок как мидлвер

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const { handleError } = require('./middlewares/handleError');
 
 const router = require('./routes/index');
 
@@ -15,6 +16,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(helmet());
 app.use(express.json());
 app.use(router);
+
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);

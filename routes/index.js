ed2893e11b7c3462fa26c2d8bdaf3ac880531798
lugvictoria <victory.senty@yandex.ru>
2023-06-08 21:57:02 +0300

    убрать поле password  и изменить валидацию avatar

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.post(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string(),
+      avatar: Joi.string().dataUri(),
     }),
   }),
   createUser,

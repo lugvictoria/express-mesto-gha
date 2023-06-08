b40f366e986b7088d1ca34ff55571427183a9fa1
lugvictoria <victory.senty@yandex.ru>
2023-06-08 16:30:38 +0300

    14 спринт добавить роуты для логина и авторизации

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { handleError } = require('../utils/handleError');
 const users = require('./users');
+const createUser = require('./users');
 const cards = require('./cards');
+const { login } = require('../controllers/login');
 
 const router = express.Router();
+
+router.post('/signup', express.json(), createUser);
+router.post('/signin', express.json(), login);
 
 router.use(users);
 router.use(cards);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const {
-  getAllUsers, getUser, createUser, updateUser, updateAvatar,
+  getAllUsers, getUser, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 const router = express.Router();
 
 router.get('/users', getAllUsers);
-router.post('/users', createUser);
 router.get('/users/:userId', getUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);

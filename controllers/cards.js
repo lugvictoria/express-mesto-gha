1f828498fa78cdef241a821452a192ec2f21f44b
lugvictoria <victory.senty@yandex.ru>
2023-06-11 13:16:10 +0300

    fix

--- controllers/cards.js
+++ controllers/cards.js
@@ -23,27 +23,37 @@ async function createCard(req, res, next) {
 }
 
 async function deleteCard(req, res, next) {
+
   try {
     const { cardId } = req.params;
 
-   const card = await Card.findById(cardId);
+     const card = await Card.findById(cardId).populate('owner');
 
-    const ownerId = card.owner.id;
-    const userId = req.user._id;
 
     if (!card) {
       throw new NotFoundError('Карточка не найдена');
     }
 
+    const ownerId = card.owner.id;
+    const userId = req.user._id;
+
+
     if (ownerId !== userId) {
       throw new ForbiddenError('Нельзя удалить чужую карточку');
     }
 
-    await Card.findByIdAndRemove(cardId); 
+
+
+    await Card.findByIdAndRemove(cardId);
+
     res.send(card);
+
   } catch (err) {
+
     next(err);
+
   }
+
 }
 
 async function putLike(req, res, next) {
